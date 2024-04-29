89fa75fa7157f013df02d19e4fea0cfc61f19d58
Tobz Coder <oluwafemioluwatobiem@gmail.com>
2024-04-29 22:48:27 +0100

    Programs

--- edusity/src/App.jsx
+++ edusity/src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Navbar from "./Components/Navbar/Navbar.jsx";
 import Hero from "./Components/Hero/Hero.jsx";
+import Programs from "./Components/Programs/Programs.jsx";
 
 const App = () => {
   return (
     <div>
       <Navbar></Navbar>
       <Hero></Hero>
+      <div className="container">
+        <Programs></Programs>
+      </div>
     </div>
   );
 };
